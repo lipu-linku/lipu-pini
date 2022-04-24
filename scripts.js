4612c9e0c0f368037a2bf40885f1ad5babee2779
Gregory Danielson <gregory.danielson3@gmail.com>
2022-04-24 12:29:19 -0500

    Make fuzzy search case sensitive

--- scripts.js
+++ scripts.js
@@ -1,7 +1,6 @@
 String.prototype.fuzzy = function (s) {
-    var hay = this.toLowerCase(), i = 0, n = -1, l;
-    s = s.toLowerCase();
-    for (; l = s[i++] ;) if (!~(n = hay.indexOf(l, n + 1))) return false;
+    var i = 0, n = -1, l;
+    for (; l = s[i++] ;) if (!~(n = this.indexOf(l, n + 1))) return false;
     return true;
 };
 
